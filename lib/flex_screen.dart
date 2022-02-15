1cef6cc9674cfeb75d9c995fb3aafb83d153740c
David. J <codewithjalloh@gmail.com>
2022-02-15 08:57:27 +0000

    containerlabel updated

--- lib/flex_screen.dart
+++ lib/flex_screen.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_layout_one/labeled_container.dart';
 
 class FlexScreen extends StatelessWidget {
   const FlexScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flexible and Expanded"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ..._header(context, "Expanded"),
+          _buildExpanded(context),
+          ..._header(context, "Flexible"),
+          _buildFlexible(context),
+        ],
+      ),
+    );
+  }
+
+  Iterable<Widget> _header(BuildContext context, String text) {
+    return [
+      SizedBox(height: 20,),
+      Text(
+        text,
+        style: Theme.of(context).textTheme.headline1,
+      ),
+    ];
+  }
+
+  Widget _buildExpanded(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: Row(
+        children: [
+          LabeledContainer(width: 100, color: Colors.green, text: "100", textColor: Colors.black), 
+          Expanded(
+              child:LabeledContainer(width: 0, color: Colors.purple, text: "The Remainder", textColor: Colors.white), ),
+          LabeledContainer(width: 40, color: Colors.green, text: "40", textColor: Colors.black),
+        ],
+      ),
+    );
+
+  }
+
+  Widget _buildFlexible(content) {
     return Container();
   }
+
+  Widget _buildFooter(BuildContext context) {
+    return Container();
+  }
+
+
 }
--- lib/labeled_container.dart
+++ lib/labeled_container.dart
@@ -10,16 +10,31 @@ class LabeledContainer extends StatelessWidget {
 
   const LabeledContainer({
     Key? key,
-    this.width,
+    required  this.width,
     this.height = double.infinity,
-    this.color,
-    @required this.text,
-    this.textColor,
+    required this.color,
+    required this.text,
+    required this.textColor,
 
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: width,
+      height: height,
+      color: color,
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(
+            color: textColor,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
   }
 }
+
+
